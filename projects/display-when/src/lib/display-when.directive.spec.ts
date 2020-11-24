1aa9f1854bde8f3562b3b6049a02a8ebf5e398fa
yinnping <ciaoing@livemail.tw>
2020-11-24 23:05:32 +0800

    Add true and false

--- projects/display-when/src/lib/display-when.directive.spec.ts
+++ projects/display-when/src/lib/display-when.directive.spec.ts
@@ -5,7 +5,7 @@ import { EDisplayWhen } from './display-when.directive';
 import { DisplayWhenModule } from './display-when.module';
 
 describe('DisplayWhenDirective', () => {
-  let fixture: ComponentFixture<any>;
+  let fixture: ComponentFixture<TestComponent>;
   afterEach(() => {
     fixture = null;
   });
@@ -17,20 +17,61 @@ describe('DisplayWhenDirective', () => {
     });
   });
 
-  it(
-    'should work in a template attribute',
-    waitForAsync(() => {
-      const template = '<span *eDisplayWhen="true">hello</span>';
-      fixture = createTestComponent(template);
-      fixture.detectChanges();
-      expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(1);
-      expect((fixture.nativeElement as HTMLElement).innerText).toEqual('hello');
-    })
-  );
+  it('should display content with true', () => {
+    const template = '<span *eDisplayWhen="true">hello</span>';
+    fixture = createTestComponent(template);
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(1);
+    expect((fixture.nativeElement as HTMLElement).innerText).toEqual('hello');
+  });
+
+  it('should display template with true', () => {
+    const template =
+      '<ng-container *eDisplayWhen="true; true temp"></ng-container><ng-template #temp><span>hello</span></ng-template>';
+    fixture = createTestComponent(template);
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(1);
+    expect((fixture.nativeElement as HTMLElement).innerText).toEqual('hello');
+  });
+
+  it(`shouldn't display content with false`, () => {
+    const template = '<span *eDisplayWhen="false">hello</span>';
+    fixture = createTestComponent(template);
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(0);
+  });
+
+  it('should display template with false', () => {
+    const template =
+      '<ng-container *eDisplayWhen="false; false temp"></ng-container><ng-template #temp><span>hello</span></ng-template>';
+    fixture = createTestComponent(template);
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(1);
+    expect((fixture.nativeElement as HTMLElement).innerText).toEqual('hello');
+  });
+
+  it('should display only one template', () => {
+    const template =
+      '<ng-container *eDisplayWhen="booleanCondition; true trueTemp; false falseTemp"></ng-container><ng-template #trueTemp><span>true</span></ng-template><ng-template #falseTemp><span>false</span></ng-template>';
+    fixture = createTestComponent(template);
+    fixture.componentInstance.booleanCondition = true;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(1);
+    expect((fixture.nativeElement as HTMLElement).innerText).toEqual('true');
+
+    fixture.componentInstance.booleanCondition = false;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('span')).length).toEqual(1);
+    expect((fixture.nativeElement as HTMLElement).innerText).toEqual('false');
+  });
 });
 
 @Component({ selector: 'test-cmp', template: '' })
-class TestComponent {}
+class TestComponent {
+  booleanCondition: boolean;
+}
 
 function createTestComponent(
   template: string
--- projects/display-when/src/lib/display-when.directive.ts
+++ projects/display-when/src/lib/display-when.directive.ts
@@ -1,22 +1,71 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  EmbeddedViewRef,
+  Input,
+  Optional,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Directive({
   selector: '[eDisplayWhen]',
 })
 export class EDisplayWhen {
+  private trueEmbeddedViewRef: EmbeddedViewRef<any>;
+  private falseEmbeddedViewRef: EmbeddedViewRef<any>;
+  private trueTemplateRef: TemplateRef<any>;
+  private falseTemplateRef: TemplateRef<any>;
   private _eDisplayWhen: boolean;
   @Input() set eDisplayWhen(condition: boolean) {
     this._eDisplayWhen = condition;
-
-    if (condition) {
-      this.viewContainerRef.createEmbeddedView(this.templateRef);
-    }
+    this.updateView();
   }
   get eDisplayWhen(): boolean {
     return this._eDisplayWhen;
   }
+
+  @Input() set eDisplayWhenTrue(templateRef: TemplateRef<any>) {
+    this.trueTemplateRef = templateRef;
+    this.trueEmbeddedViewRef = null;
+    this.updateView();
+  }
+
+  @Input() set eDisplayWhenFalse(templateRef: TemplateRef<any>) {
+    this.falseTemplateRef = templateRef;
+    this.falseEmbeddedViewRef = null;
+    this.updateView();
+  }
+
   constructor(
     private viewContainerRef: ViewContainerRef,
-    private templateRef: TemplateRef<any>
-  ) {}
+    templateRef: TemplateRef<any>
+  ) {
+    this.trueTemplateRef = templateRef;
+  }
+
+  private updateView(): void {
+    if (this.eDisplayWhen) {
+      if (!this.trueEmbeddedViewRef) {
+        this.viewContainerRef.clear();
+        this.falseEmbeddedViewRef = null;
+
+        if (this.trueTemplateRef) {
+          this.trueEmbeddedViewRef = this.viewContainerRef.createEmbeddedView(
+            this.trueTemplateRef
+          );
+        }
+      }
+    } else {
+      if (!this.falseEmbeddedViewRef) {
+        this.viewContainerRef.clear();
+        this.trueEmbeddedViewRef = null;
+
+        if (this.falseTemplateRef) {
+          this.falseEmbeddedViewRef = this.viewContainerRef.createEmbeddedView(
+            this.falseTemplateRef
+          );
+        }
+      }
+    }
+  }
 }
